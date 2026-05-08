9e65287ad31d8dfde375afe8541b432ca786ce00
Leena <panchalleena8839@gmail.com>
2026-05-08 22:50:04 +0530

    Initial Commit

---
+++ src/com/java8practice/lambda/Main.java
@@ -0,0 +1,8 @@
+package com.java8practice.lambda;
+
+public class Main {
+
+    private void sayHello(){
+        System.out.println("Hello !");
+    }
+}
---
+++ src/com/java8practice/stream/StreamPractice.java
@@ -0,0 +1,4 @@
+package com.java8practice.stream;
+
+public class StreamPractice {
+}
