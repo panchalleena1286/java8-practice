e9e7483e9df0060d3c4986f42271c88f9ad6a3f6
Leena <panchalleena8839@gmail.com>
2026-05-09 23:05:29 +0530

    Initial Commit

---
+++ src/com/java8practice/stream/Example2.java
@@ -0,0 +1,25 @@
+package com.java8practice.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Example2 {
+
+    public static void main(String[] args){
+        List<String> list = Arrays.asList("apple", "banana", "cherry");
+
+        Stream<String> myStream = list.stream();
+
+        String[] array = {"apple", "banana", "cherry"};
+        Stream<String> stream = Arrays.stream(array);
+
+        //create direct stream
+        Stream<Integer> str = Stream.of(1,2,3);
+
+        Stream<Integer> str1 = Stream.iterate(0, n -> n+1).limit(100);
+
+        Stream<Integer> limit = Stream.generate(() -> (int) Math.random() * 100).limit(5);
+
+    }
+}
